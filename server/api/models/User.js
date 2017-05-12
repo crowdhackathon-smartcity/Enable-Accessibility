fa46b750678712ecf6d1b80efab7dbd9a06f1961
blackspirit96 <karatakis@csd.auth.gr>
2017-05-12 20:51:56 +0300

    Add user model

---
+++ server/api/models/User.js
@@ -0,0 +1,30 @@
+/**
+ * User.js
+ *
+ * @description :: Users model
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    username: {
+      type: 'string',
+      required: true
+    },
+    password: {
+      type: 'string',
+      required: true
+    },
+    points: {
+      type: 'integer',
+      defaultsTo: 0,
+      required: true
+    },
+    type: {
+      type: 'string',
+      enum: ['civilian', 'manicipality', 'business'],
+      required: true
+    }
+  }
+};
