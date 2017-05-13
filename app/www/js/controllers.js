61637a6728d8ba64a4c9ff64fd6b8f6e02b58d01
blackspirit96 <karatakis@csd.auth.gr>
2017-05-13 10:21:21 +0300

    ionic app init

---
+++ app/www/js/controllers.js
@@ -0,0 +1,146 @@
+angular.module('app.controllers', [])
+  
+.controller('userCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('mapCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('reportCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('smartAccessibilityCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('settingsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('advertisementCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('municipalityCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('redeemPointsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('feedbackCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('reportListCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('reviewListCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('navigationMapCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('accessibilityMapCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('liveTrackingMapCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('vouchersPromotionsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('addVoucherPromotionCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ app/www/js/routes.js
@@ -0,0 +1,165 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('smartAccessibility.user', {
+    url: '/user',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/user.html',
+        controller: 'userCtrl'
+      }
+    }
+  })
+
+  .state('smartAccessibility.map', {
+    url: '/map',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/map.html',
+        controller: 'mapCtrl'
+      }
+    }
+  })
+
+  .state('smartAccessibility.report', {
+    url: '/report',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/report.html',
+        controller: 'reportCtrl'
+      }
+    }
+  })
+
+  .state('smartAccessibility', {
+    url: '/list-menu',
+    templateUrl: 'templates/smartAccessibility.html',
+    controller: 'smartAccessibilityCtrl'
+  })
+
+  .state('smartAccessibility.settings', {
+    url: '/settings',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/settings.html',
+        controller: 'settingsCtrl'
+      }
+    }
+  })
+
+  .state('smartAccessibility.advertisement', {
+    url: '/advertisement',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/advertisement.html',
+        controller: 'advertisementCtrl'
+      }
+    }
+  })
+
+  .state('smartAccessibility.municipality', {
+    url: '/municipality',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/municipality.html',
+        controller: 'municipalityCtrl'
+      }
+    }
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('smartAccessibility.redeemPoints', {
+    url: '/redeem',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/redeemPoints.html',
+        controller: 'redeemPointsCtrl'
+      }
+    }
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+  .state('feedback', {
+    url: '/feedback',
+    templateUrl: 'templates/feedback.html',
+    controller: 'feedbackCtrl'
+  })
+
+  .state('smartAccessibility.reportList', {
+    url: '/report list',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/reportList.html',
+        controller: 'reportListCtrl'
+      }
+    }
+  })
+
+  .state('smartAccessibility.reviewList', {
+    url: '/reviews list',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/reviewList.html',
+        controller: 'reviewListCtrl'
+      }
+    }
+  })
+
+  .state('navigationMap', {
+    url: '/mockmap_nav',
+    templateUrl: 'templates/navigationMap.html',
+    controller: 'navigationMapCtrl'
+  })
+
+  .state('accessibilityMap', {
+    url: '/mockmap_acc',
+    templateUrl: 'templates/accessibilityMap.html',
+    controller: 'accessibilityMapCtrl'
+  })
+
+  .state('liveTrackingMap', {
+    url: '/mockmap_live',
+    templateUrl: 'templates/liveTrackingMap.html',
+    controller: 'liveTrackingMapCtrl'
+  })
+
+  .state('smartAccessibility.vouchersPromotions', {
+    url: '/page21',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/vouchersPromotions.html',
+        controller: 'vouchersPromotionsCtrl'
+      }
+    }
+  })
+
+  .state('addVoucherPromotion', {
+    url: '/page22',
+    templateUrl: 'templates/addVoucherPromotion.html',
+    controller: 'addVoucherPromotionCtrl'
+  })
+
+$urlRouterProvider.otherwise('/list-menu/map')
+
+  
+
+});
